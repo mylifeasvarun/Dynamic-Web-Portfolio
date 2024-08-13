f873f66300740621d1e1b6e49cf93d39e98f759a
saigangasani <sgangasani@ufl.edu>
2024-08-13 17:25:25 -0400

    Static -> Dynamic Conversion
    -Setup Backend Server
    -Updated .env
    -Fixed DB connection
    -Pushed Data to DB
    -Setup API Endpoint to get Portfolio Data

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import Home from "./pages/Home/Home.js";
-import { useState } from "react";
 import Loader from "./components/Loader.js";
 
 function App() {
   const [ShowLoading, SetShowLoading] = useState(false);
+
+  const getportfolioData = async () => {
+    try {
+      const response = await axios.get("/api/portfolio/get-portfolio-data");
+      console.log(response.data);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    getportfolioData();
+  }, []);
+
   return (
     <BrowserRouter>
       {ShowLoading ? <Loader /> : null}
