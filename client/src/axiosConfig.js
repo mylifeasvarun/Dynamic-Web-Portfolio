af811984d772f69c33b80365cdc53911e86968c8
saigangasani <sgangasani@ufl.edu>
2025-02-05 15:53:19 -0600

    Re-structured

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -1,13 +1,7 @@
 import axios from "axios";
 
-const baseURL =
-  process.env.NODE_ENV === "production"
-    ? "https://dynamic-web-portfolio-itcw.vercel.app/api"
-    : "http://localhost:5001/api";
-
 const instance = axios.create({
-  baseURL,
-  withCredentials: true,
+  baseURL: "/api",
 });
 
 export default instance;
