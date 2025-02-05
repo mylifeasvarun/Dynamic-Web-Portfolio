2cde49cd4a50e754a640cd18c24234f5e2d7224c
saigangasani <sgangasani@ufl.edu>
2025-02-05 17:08:15 -0600

    Dev & Prod Dual Support

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-const instance = axios.create({
-  baseURL: "/api",
-});
+const baseURL =
+  process.env.NODE_ENV === "development" ? "http://localhost:5001/api" : "/api";
+
+const instance = axios.create({ baseURL });
 
 export default instance;
