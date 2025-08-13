a1f5a43996a8ead438946d836baa7743759eb528
Sai Varun Reddy Gangasani <sgangasani@ufl.edu>
2025-08-12 21:39:57 -0700

    Refactor: Replace dotlottie-player with lottie-player
    --add refs in About and Contact components

--- client/src/pages/Home/About.js
+++ client/src/pages/Home/About.js
@@ -6,19 +6,22 @@ function About() {
   const { portfolioData } = useSelector((state) => state.root);
   const { about } = portfolioData;
   const { skills, lottieURL, description1, description2 } = about;
+
+  const playerRef = React.useRef(null);
   return (
     <div className="bg-primary py-10">
       <SectionTitle title="About" />
 
       <div className="flex items-center gap-10 sm:flex-col">
         <div className="h-[50vh] w-1/2 sm:w-full">
-          <dotlottie-player
+          <lottie-player
+            ref={playerRef}
             src={lottieURL}
             background="transparent"
             speed="1"
             loop
             autoplay
-          ></dotlottie-player>
+          ></lottie-player>
         </div>
         <div className="flex flex-col gap-5 w-1/2 sm:w-full">
           <p className="text-white text-justify">{description1 || ""}</p>
--- client/src/pages/Home/Contact.js
+++ client/src/pages/Home/Contact.js
@@ -5,6 +5,7 @@ import { useSelector } from "react-redux";
 function Contact() {
   const { portfolioData } = useSelector((state) => state.root);
   const { contact } = portfolioData;
+  const playerRef = React.useRef(null);
   return (
     <div>
       <SectionTitle title="Say Hello" />
@@ -23,13 +24,14 @@ function Contact() {
           <p className="text-white">{"}"}</p>
         </div>
         <div className="h-[400px]">
-          <dotlottie-player
+          <lottie-player
+            ref={playerRef}
             src="https://lottie.host/30f2dc13-9689-4fd9-ac9a-4bd0d3e8101e/L6oeLYesvW.json"
             background="transparent"
             speed="1"
             loop
             autoplay
-          ></dotlottie-player>
+          ></lottie-player>
         </div>
       </div>
     </div>
